1e339602ab36b89b23850dd8b1b71ae0269954b7
andrew yeomans <andrew@dcsdigital.co.uk>
2024-05-06 11:43:54 +0100

    update the routes

--- routes/products-routes.php
+++ routes/products-routes.php
@@ -8,7 +8,6 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/orders', [OrdersController::class, 'index'])->name('orders.index');
         Route::get('/products', [ProductsController::class, 'index'])->name('products.index');
     // Route::get('/products/create', [ProductsController::class, 'create'])->name('products.create');
     // Route::post('/products', [ProductsController::class, 'store'])->name('products.store');
