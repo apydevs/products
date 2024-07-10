03ac2842b60a00762c91351b97a3eee664f3cb16
andrew yeomans <andrew@dcsdigital.co.uk>
2024-07-10 11:05:17 +0100

    product table review

--- src/Livewire/ProductsTable.php
+++ src/Livewire/ProductsTable.php
@@ -34,25 +34,25 @@ class ProductsTable extends DataTableComponent
                 ->sortable()->searchable(),
             Column::make('title', 'title')
                 ->sortable()->searchable(),
-            Column::make('type', 'type')
-                ->sortable()->searchable(),
+//            Column::make('type', 'type')
+//                ->sortable()->searchable(),
             Column::make('manufacture', 'manufacture')
                 ->sortable()->searchable(),
             Column::make('model', 'model')
                 ->sortable()->searchable(),
-            Column::make('quantity', 'quantity')
-                ->sortable()->searchable(),
-            Column::make('low_quantity', 'low_quantity')
-                ->sortable()->searchable(),
+//            Column::make('quantity', 'quantity')
+//                ->sortable()->searchable(),
+//            Column::make('low_quantity', 'low_quantity')
+//                ->sortable()->searchable(),
 
             Column::make('price', 'price')
                 ->sortable()->searchable(),
-            Column::make('supplier_ref', 'supplier_ref')
-                ->sortable()->searchable(),
+//            Column::make('supplier_ref', 'supplier_ref')
+//                ->sortable()->searchable(),
             Column::make('category_id', 'category_id')
                 ->sortable()->searchable(),
-            Column::make('Pre ordered', 'pre-order')
-                ->sortable()->searchable(),
+//            Column::make('Pre ordered', 'pre-order')
+//                ->sortable()->searchable(),
             Column::make('status', 'status')
                 ->sortable()->searchable(),
             Column::make('Action')
