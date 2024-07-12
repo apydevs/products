b05cd4756732b8d44b6d91c3ada38abddca06bb4
andrew yeomans <andrew@dcsdigital.co.uk>
2024-07-12 09:11:19 +0100

    added contact and edit

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -58,7 +58,7 @@
                             <div class="mt-2">
                                 <select id="category" name="category"  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
                                     @foreach($categories as $cat)
-                                        <option {{ old('category', $product->category_id) ==  $cat->id  ? 'selected' : '' }} class="capitalize" value="{{ $cat->id }}">{{ $cat->name }}</option>
+                                        <option {{ old('category', $cat->category_id) ==  $cat->id  ? 'selected' : '' }} class="capitalize" value="{{ $cat->id }}">{{ $cat->name }}</option>
                                     @endforeach
                                 </select>
                                 @error('category')
